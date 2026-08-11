762d5a30ca32aad1f1723971f76572f3ed7cda07
Ben Hamlin <hamlinb@galois.com>
2026-08-11 01:39:01 -0700

    Remove stale Spec/UAKE/SecurityLemmas.lean

--- PQXDH/Spec/UAKE/Security.lean
+++ PQXDH/Spec/UAKE/Security.lean
@@ -3,7 +3,7 @@ Copyright (c) 2026 Galois Inc. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Ben Hamlin
 -/
-import PQXDH.Spec.UAKE.SecurityLemmas
+import PQXDH.Spec.UAKE.Defs
 
 /-!
 # Security Theorems for PQXDH Spec-based Implementation
